38800046b29b4a4b4fa68212196ee90d4b512af6
M1saaaa <est.campo.tomas@latecnicalf.com.ar>
2023-04-27 18:20:53 -0300

    CargaProyecto

---
+++ app/Models/Mesa.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mesa extends Model
+{
+    use HasFactory;
+    protected $filiable = [
+        'capacidad',
+        'usuario',
+    ];
+
+   public function mesas()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
+
